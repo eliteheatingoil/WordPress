d50d74c55909675489fc596bc118417a25d743be
eliteheatingoil <eliteheatingoil@gmail.com>
2017-09-09 11:36:15 -0300

    Started on theme.

---
+++ wp-content/themes/eliteheatingoil/footer.php
@@ -0,0 +1,11 @@
+    </div>  
+		<footer class="blog-footer">
+    </footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+    <script src="<?php echo get_bloginfo( 'template_directory' );?>/dist/js/main.min.js"></script>
+<?php wp_footer(); ?> 
+  </body>
+</html>
