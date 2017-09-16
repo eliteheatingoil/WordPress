9e01f9ccf912246d9727551bf2bcbd210e0a3995
eliteheatingoil <eliteheatingoil@gmail.com>
2017-09-16 12:10:06 -0300

    removed js.

--- wp-content/themes/eliteheatingoil/js/main.js
+++ wp-content/themes/eliteheatingoil/js/main.js
@@ -9,39 +9,39 @@ $(document).ready(function(){
     autoplaySpeed: 3000,
   });
 
-  /// ANIMATE CARDS ON LOAD ///
-  $(".card").each(function() {
-    if ($(this).isOnScreen()) {
-      $(this).addClass("animated fadeInUp");
-      $(this).css('visibility', 'visible');
-    }
-  });
+  // /// ANIMATE CARDS ON LOAD ///
+  // $(".card").each(function() {
+  //   if ($(this).isOnScreen()) {
+  //     $(this).addClass("animated fadeInUp");
+  //     $(this).css('visibility', 'visible');
+  //   }
+  // });
 
-  /// ANIMATE SPECIAL OFFER ON LOAD ///
-  if ($('.special-offer').isOnScreen()) {
-    $('.special-offer').addClass("animated fadeIn");
-    $('.special-offer').css('visibility', 'visible');
-  }
+  // /// ANIMATE SPECIAL OFFER ON LOAD ///
+  // if ($('.special-offer').isOnScreen()) {
+  //   $('.special-offer').addClass("animated fadeIn");
+  //   $('.special-offer').css('visibility', 'visible');
+  // }
 
 
-  /// ANIMATE CARDS AND SPECIAL OFFER ON SCROLL ///
-  $(window).on('scroll', function() {
-    $(".card").each(function() {
-        if (isScrolledIntoView($(this))) {
-          if (!$(this).hasClass('animated')){
-            $(this).addClass("animated fadeInUp");
-            $(this).css('visibility', 'visible');
-          }
-        }
-    });
+  // /// ANIMATE CARDS AND SPECIAL OFFER ON SCROLL ///
+  // $(window).on('scroll', function() {
+  //   $(".card").each(function() {
+  //       if (isScrolledIntoView($(this))) {
+  //         if (!$(this).hasClass('animated')){
+  //           $(this).addClass("animated fadeInUp");
+  //           $(this).css('visibility', 'visible');
+  //         }
+  //       }
+  //   });
 
-    if (isScrolledIntoView($('.special-offer'))) {
-      if( !$('.special-offer').hasClass('animated')){
-        $('.special-offer').addClass("animated fadeIn");
-        $('.special-offer').css('visibility', 'visible');
-      }
-    }
-  });
+  //   if (isScrolledIntoView($('.special-offer'))) {
+  //     if( !$('.special-offer').hasClass('animated')){
+  //       $('.special-offer').addClass("animated fadeIn");
+  //       $('.special-offer').css('visibility', 'visible');
+  //     }
+  //   }
+  // });
 
   /// BURGER MENU CHANGE ///
   $('.burger-menu-btn').click(function(){
@@ -53,33 +53,32 @@ $(document).ready(function(){
         $('.mobile-nav').addClass('open');
       }
 
-      // $(this).addClass('burger-disabled');
-      // setTimeout(function() {
-      //     $(this).removeClass('burger-disabled');
-      //     alert('stuff');
-      // }, 200);
+      $(this).addClass('burger-disabled');
+      setTimeout(function() {
+          $(this).removeClass('burger-disabled');
+      }, 200);
 
   });
 
 });
 
 
-function isScrolledIntoView(elem) {
-  var docViewTop = $(window).scrollTop();
-  var docViewBottom = docViewTop + $(window).height();
+// function isScrolledIntoView(elem) {
+//   var docViewTop = $(window).scrollTop();
+//   var docViewBottom = docViewTop + $(window).height();
 
-  var elemTop = $(elem).offset().top;
-  var elemBottom = elemTop + $(elem).height();
+//   var elemTop = $(elem).offset().top;
+//   var elemBottom = elemTop + $(elem).height();
 
-  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
+//   return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+// }
 
-$.fn.isOnScreen = function(){
-    var viewport = {};
-    viewport.top = $(window).scrollTop();
-    viewport.bottom = viewport.top + $(window).height();
-    var bounds = {};
-    bounds.top = this.offset().top;
-    bounds.bottom = bounds.top + this.outerHeight();
-    return ((bounds.top <= viewport.bottom) && (bounds.bottom >= viewport.top));
-};
+// $.fn.isOnScreen = function(){
+//     var viewport = {};
+//     viewport.top = $(window).scrollTop();
+//     viewport.bottom = viewport.top + $(window).height();
+//     var bounds = {};
+//     bounds.top = this.offset().top;
+//     bounds.bottom = bounds.top + this.outerHeight();
+//     return ((bounds.top <= viewport.bottom) && (bounds.bottom >= viewport.top));
+// };
