9ecf554a94fd6237bc7261a572b35b802b008cdd
eliteheatingoil <eliteheatingoil@gmail.com>
2017-09-24 13:21:39 -0300

    Added plugins for custom post types and custom fields. Today's price template now uses dynamin price.

--- wp-content/themes/eliteheatingoil/partials/todays-price.php
+++ wp-content/themes/eliteheatingoil/partials/todays-price.php
@@ -1,3 +1,11 @@
+<?php
+$the_query = new WP_Query(array(
+  'post_type'			=> 'oil_price',
+  'posts_per_page'	=> 1,
+  'order'				=> 'DESC'
+));
+?>
+
 <section class="row price-slice animated fadeIn">
   <div class="container">
       <div class="col-sm-12">
@@ -8,7 +16,16 @@
             </div>
             <div class="col-sm-12">
               <div class="col-xs-9 oil-price-container">
-                <h3 class="h1 oil-price">$0.7690</h3>
+                <h3 class="h1 oil-price">
+
+                <?php if( $the_query->have_posts() ): ?>
+                  <?php while( $the_query->have_posts() ) : $the_query->the_post(); 
+                    echo '$' . get_field('price');
+                  endwhile;
+                endif;
+                ?>
+                
+                </h3>
               </div>
               <div class="col-xs-3 per-litre-container">
                 <div class="row">Per Litre</div>
