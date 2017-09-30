96b9c5e9426844513c94e6f7e501a23a51df938c
eliteheatingoil <eliteheatingoil@gmail.com>
2017-09-30 19:28:16 -0300

    Fixed page template loop.

--- wp-content/themes/eliteheatingoil/page.php
+++ wp-content/themes/eliteheatingoil/page.php
@@ -4,22 +4,19 @@
 <div class="container">
 
 <div class="page-content col-xs-12 col-sm-10 col-sm-offset-1">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
-        <li class="breadcrumb-item active"><?php the_title(); ?></li>
-    </ol>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
+            <li class="breadcrumb-item active"><?php the_title(); ?></li>
+        </ol>
 
-    <div class="page-title">
-        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-    </div>
-    <div class="page-body">
-        <?php
-            // Start the loop.
-            while ( have_posts() ) : the_post();
-            the_content(); 
-            endwhile;
-        ?>
-    </div>
+        <div class="page-title">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </div>
+        <div class="page-body">
+            <?php the_content(); ?> 
+        </div>
+    <?php endwhile; ?>
 </div>
 
 </div>
