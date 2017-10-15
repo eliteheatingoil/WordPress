0153c33aabcba699ff290aa6a2c04cf06c6b477f
eliteheatingoil <eliteheatingoil@gmail.com>
2017-10-15 09:47:23 -0300

    Hiding Yoast metabox.

--- wp-content/themes/eliteheatingoil/functions.php
+++ wp-content/themes/eliteheatingoil/functions.php
@@ -4,3 +4,13 @@ function register_main_menu() {
   register_nav_menu('main-menu',__( 'Main Menu' ));
 }
 add_action( 'init', 'register_main_menu' );
+
+add_action('add_meta_boxes', 'yoast_is_toast', 99);
+function yoast_is_toast(){
+    //capability of 'manage_plugins' equals admin, therefore if NOT administrator
+    //hide the meta box from all other roles on the following 'post_type' 
+    //such as post, page, custom_post_type, etc
+    remove_meta_box('wpseo_meta', 'post', 'normal');
+    remove_meta_box('wpseo_meta', '', 'normal');
+    remove_meta_box('wpseo_meta', 'oil_price', 'normal');
+}
